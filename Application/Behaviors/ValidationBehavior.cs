f7273269341a4e179f9105bc1088b986dda34802
AbdullahSoftech <abdullah.softech@gmail.com>
2026-04-29 11:39:17 +0500

    use primary constructor in validation behavior pipleline

--- Application/Behaviors/ValidationBehavior.cs
+++ Application/Behaviors/ValidationBehavior.cs
@@ -3,20 +3,11 @@ using MediatR;
 
 namespace Application.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> _validators) 
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
-    private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
-    {
-        _validators = validators;
-    }
-
-    public async Task<TResponse> Handle(
-        TRequest request,
-        RequestHandlerDelegate<TResponse> next,
-        CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (!_validators.Any())
             return await next(cancellationToken);
